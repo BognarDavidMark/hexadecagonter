4423c9897fdad7e591c324899abc065908ca3967
unknown <bognardavidmark@gmail.com>
2022-03-08 09:24:07 +0100

    Kezdes

---
+++ hexadecagonter.java
@@ -0,0 +1,29 @@
+/*
+ * File: hexadecagonter.java
+ * Author: Bognár Dávid Márk
+ * Copyright: Bognár Dávid Márk, 2022.03.08.
+ * Group: Szoft I/N
+ * Date: 2022.03.08.
+ * Github: 
+ * Licenc: GNU GPL
+ */
+
+import java.util.Scanner;
+
+public class hexadecagonter {
+	public static void main (String[] args) {
+/*		
+	System.out.println("Bognár Dávid Márk, Szoft I/N, 2022.03.08.");
+	System.out.println("Hexadecagon területet számol");
+*/		
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Oldalhossz: ");
+	double oldal = scan.nextDouble();
+	
+//	double terulet = 4*(oldal*oldal)*cot*(Math.PI/16);
+	
+//	System.out.println("Az alakzat területe: " + terulet);
+	
+	}
+}
+
